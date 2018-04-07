8f7c4b356c5100d411f1754faa50fa6e4866c65b
Alex Barton <alexmbarton@gmail.com>
2018-04-07 23:53:01 +0100

    MIT kickoff

---
+++ Python/simple_conditions.py
@@ -0,0 +1,13 @@
+# Grab input
+input = int(raw_input("Int me: "))
+
+# Write the output
+if input % 2 == 0:
+    if input % 3 == 0:
+        print('Divisible by 2 and 3')
+    else:
+        print('Divisible by 2 and not by 3')
+elif input % 3 == 0:
+    print('Divisible by 3 and not by 2')
+else:
+    print('Not divisible by 2 nor 3')
